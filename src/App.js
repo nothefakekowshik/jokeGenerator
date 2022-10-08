27e509a7f0a743f24e528c3f01ef45b848083e79
notthefakekowshik <kowsik02@gmail.com>
2022-10-08 08:18:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import ListData from "./components/ListData"
+
+function App() {
+  return (
+    <div className="App">
+      <ListData/>
+    </div>
+  );
+}
+
+export default App;
